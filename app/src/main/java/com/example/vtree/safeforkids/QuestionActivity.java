eac87c39548e1ee5468a2f3f175b895852ec4544
hieuntph03778 <hieuntph03778@fpt.edu.vn>
2016-09-08 10:21:07 +0700

    update quest

--- app/src/main/java/com/example/vtree/safeforkids/QuestionActivity.java
+++ app/src/main/java/com/example/vtree/safeforkids/QuestionActivity.java
@@ -1,7 +1,12 @@
 package com.example.vtree.safeforkids;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_layout);
+        Button btnShould,btnShouldnt;
+        ImageView image = new ImageView(this);
+        image.setImageResource(R.drawable.medal_gold_2);
+        btnShould = (Button) findViewById(R.id.btnShould);
+        btnShouldnt = (Button) findViewById(R.id.btnShouldnt);
+        final AlertDialog.Builder builder =
+                new AlertDialog.Builder(this);
+        builder.setMessage("Message above the image");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+        //builder.setView(image);
+        btnShould.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.create().show();
+            }
+        });
+        btnShouldnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.create().show();
+            }
+        });
     }
 }

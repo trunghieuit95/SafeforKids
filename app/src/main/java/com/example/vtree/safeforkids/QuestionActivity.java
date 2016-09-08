cf25004c1a2372d4cfbd8fb81ce08487a2050e41
hieuntph03778 <hieuntph03778@fpt.edu.vn>
2016-09-08 16:33:32 +0700

    update quest

--- app/src/main/java/com/example/vtree/safeforkids/QuestionActivity.java
+++ app/src/main/java/com/example/vtree/safeforkids/QuestionActivity.java
@@ -1,13 +1,9 @@
 package com.example.vtree.safeforkids;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -16,31 +12,19 @@ public class QuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_layout);
         Button btnShould,btnShouldnt;
-        ImageView image = new ImageView(this);
-        image.setImageResource(R.drawable.medal_gold_2);
         btnShould = (Button) findViewById(R.id.btnShould);
         btnShouldnt = (Button) findViewById(R.id.btnShouldnt);
-        final AlertDialog.Builder builder =
-                new AlertDialog.Builder(this);
-        builder.setMessage("Message above the image");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent intent = new Intent(QuestionActivity.this, MainActivity.class);
-                                startActivity(intent);
-                            }
-                        });
-        builder.setView(image);
+        final Customdialog alert = new Customdialog();
         btnShould.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                builder.create().show();
+                alert.showDialog(QuestionActivity.this,"Nên");
             }
         });
         btnShouldnt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                builder.create().show();
+                alert.showDialog(QuestionActivity.this,"Không Nên");
             }
         });
     }
